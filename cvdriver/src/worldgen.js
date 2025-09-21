b87f58b26e66208604fd1481e893355eb114aedf
Alan Munschy <alanmunschy@gmail.com>
2025-09-20 22:04:56 -0400

    generating 100 road segments at a time now

--- cvdriver/src/worldgen.js
+++ cvdriver/src/worldgen.js
@@ -9,7 +9,7 @@ function generateRoadSchematic(initialX, initialY, initialZ = 0, initialAngle = 
     let y = initialY;
     let angle = initialAngle; // Start from the provided initial angle instead of 0
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 100; i++) {
         z -= 20;
         angle += (Math.random() - 0.5) * 3; // random small turn
         x += Math.sin(angle) * 6; // curve effect
